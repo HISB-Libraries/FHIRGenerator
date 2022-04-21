b08718ba21e32196de6e4ba569e83079c94b19db
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2022-04-21 13:16:15 -0400

    Fixing to include us core files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = ["fhir.resources>=6.2.2", "Faker>=13.3.4", "orjson>=3.6.8"]
 
 setuptools.setup(
     name="fhirgenerator",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrew Stevens",
     author_email="andrew.stevens@gtri.gatech.edu",
     description="A package to generate FHIR Resources using a configuration file",
@@ -24,7 +24,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "fhirgenerator"},
-    packages=setuptools.find_packages(where="fhirgenerator"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.10",
+    package_data={'fhirgenerator.tests.input': ['config.json']}
 )
