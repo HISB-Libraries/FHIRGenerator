ccb1b8f8af2a022e1fd3b8b072b37117e6095826
Ellie Shivers <elliemshivers@gmail.com>
2022-05-13 14:28:33 -0400

    Refactored observation and added component

--- fhirgenerator/resources/r4/observation.py
+++ fhirgenerator/resources/r4/observation.py
@@ -2,7 +2,8 @@
 
 import uuid
 import random
-from fhir.resources.observation import Observation
+from fhir.resources.observation import Observation, ObservationComponent
+from fhir.resources.quantity import Quantity
 
 from fhirgenerator.helpers.helpers import makeRandomDate
 
@@ -16,8 +17,60 @@ def generateObservation(resource_detail: dict, patient_id: str, start_date: str,
 
     random_date = makeRandomDate(start_date, days)
 
-    if 'enumSetList' in resource_detail:
-        enum_set_list = resource_detail['enumSetList']
+    value_x_type, value_x_value = handleValueTypes(resource_detail)
+
+    observation_data = {
+        'id': observation_id,
+        'status': 'final',
+        'code': {
+            'coding': [
+                observation_code
+            ]
+        },
+        'subject': {
+            'reference': f'Patient/{patient_id}'
+        },
+        'effectiveDateTime': str(random_date),
+        f'value{value_x_type}': value_x_value
+    }
+    
+    if 'valueNone' in observation_data:
+        del observation_data['valueNone']
+
+    if 'profile' in resource_detail:
+        observation_data['meta'] = {}
+        observation_data['meta']['profile'] = resource_detail['profile']
+
+    if 'components' in resource_detail:
+        observation_data['component'] = []
+        for component_details in resource_detail['components']:
+            observation_data['component'].append(generateObservationComponent(component_details))
+
+    observation_resource = Observation(**observation_data).dict()
+    return observation_resource
+
+
+def generateObservationComponent(component_detail):
+    component_code = random.choice(component_detail['codes'])
+
+    value_x_type, value_x_value = handleValueTypes(component_detail)
+
+    component_data = {
+        'code': {
+            'coding': [
+                component_code
+            ]
+        },
+        f'value{value_x_type}': value_x_value
+    }
+
+    component = ObservationComponent(**component_data).dict()
+    return component
+
+
+def handleValueTypes(detail, decimal_value=None):
+    if 'enumSetList' in detail:
+        enum_set_list = detail['enumSetList']
         if isinstance(enum_set_list[0], dict):
             if 'system' in enum_set_list[0]:
                 value_x_type = 'CodeableConcept'
@@ -39,57 +92,56 @@ def generateObservation(resource_detail: dict, patient_id: str, start_date: str,
         else:
             value_x_type = 'String'
             value_x_value = random.choice(enum_set_list)
-    elif 'minValue' in resource_detail and 'maxValue' in resource_detail:
-        min_value = resource_detail['minValue']
-        max_value = resource_detail['maxValue']
-        if 'decimalValue' in resource_detail:
-            decimal_value = resource_detail['decimalValue']
-            value_x_type = 'Quantity'
-            value_x_value = {
-                'value': round(random.uniform(min_value, max_value), decimal_value)
-            }
-            if decimal_value == 0:
-                value_x_value['value'] = float(value_x_value['value'])
-            if 'unit' in resource_detail:
-                system, code, display = resource_detail['unit'].split('^')
-                value_x_value['unit'] = display
-                value_x_value['system'] = system
-                value_x_value['code'] = code
-        else:
-            if 'unit' in resource_detail:
-                value_x_type = 'Quantity'
-                system, code, display = resource_detail['unit'].split('^')
-                value_x_value = {
-                    'value': round(random.uniform(min_value, max_value)),
-                    'unit': display,
-                    'system': system,
-                    'code': code
-                }
+    elif 'minValue' in detail and 'maxValue' in detail:
+        # Quantity or Integer Value
+        min_value = detail['minValue']
+        max_value = detail['maxValue']
+        if 'decimalValue' in  detail:
+            decimal_value = detail['decimalValue']
+
+        if 'unit' in detail:
+            value_x_type, value_x_value = createValueQuantity(min_value, max_value, detail['unit'], decimal_value)
+        else: 
+            if decimal_value is not None:
+                value_x_type, value_x_value = createValueQuantity(min_value, max_value, None, decimal_value)
             else:
-                value_x_type = 'Integer'
-                value_x_value = round(random.uniform(min_value, max_value))
+                value_x_type, value_x_value = createValueInteger(min_value, max_value)
     else:
         print("Warning: There was no enumSetList or (minValue and maxValue) in your configuration for this Observation. This Observation will not have a value[x].")
         value_x_type = 'None'
         value_x_value = ''
+    
+    return value_x_type, value_x_value
 
-    observation_data = {
-        'id': observation_id,
-        'status': 'final',
-        'code': {
-            'coding': [
-                observation_code
-            ]
-        },
-        'subject': {
-            'reference': f'Patient/{patient_id}'
-        },
-        'effectiveDateTime': str(random_date),
-        f'value{value_x_type}': value_x_value
-    }
 
-    if 'valueNone' in observation_data:
-        del observation_data['valueNone']
 
-    observation_resource = Observation(**observation_data).dict()
-    return observation_resource
+def createValueInteger(min_value, max_value):
+    type = "Integer"
+    value = int(round(random.uniform(min_value, max_value)))
+    return type, value
+
+
+def createValueQuantity(min_value, max_value, unit_coding=None, decimal=None):
+    type = "Quantity"
+
+    value = random.uniform(min_value, max_value)
+    if decimal is not None:
+        value = round(value, decimal)
+    else:
+        value = int(value)
+
+    if unit_coding is not None:
+        system, code, display = unit_coding.split('^')
+        quantity_data = {
+            'value': value,
+            'unit': display,
+            'system': system,
+            'code': code
+        }
+    else:
+        quantity_data = {
+            'value': value
+        }
+
+    quantity = Quantity(**quantity_data).dict()
+    return type, quantity
